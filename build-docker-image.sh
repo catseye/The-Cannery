5cd5522caec2cd9df6d1529b00fadad132d8784b
Chris Pressey <cpressey@catseye.tc>
2019-05-17 14:50:48 +0100

    Build script looks in SHELF_PATH to find SOURCE.

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
 # Common script for building a Docker image.
-# Intended to be called from something else that sets up some env vars first:
+# Intended to be called from something else that sets up some env vars first
+# (using Falderal as an example here, obviously change these as necessary):
 
 # SOURCE=Falderal
 # IMAGENAME=falderal
 # VERSION=0.13
 
+shelf_pwd() {
+    name="$1"
+    path=`echo "$SHELF_PATH" | sed -e 's/:/ /g'`
+    for dir in $path; do
+        if [ -d "$dir/$name" ]; then
+            echo "$dir/$name"
+            return 0
+        fi
+    done
+    return 1
+}
+
+if shelf_pwd ${SOURCE}; then
+  SRCDIR=`shelf_pwd ${SOURCE}`
+else
+  echo "Please set SHELF_PATH to a directory where the source '${SOURCE}' can be found."
+  exit 1
+fi
+
 ORGNAME=catseye
-SRCDIR=/home/cpressey/catseye/${SOURCE}
 DOCKERFILE=`pwd`/Dockerfile
 
 find ${SRCDIR} -name '*.pyc' -exec rm {} \;
