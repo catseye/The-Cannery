53ab0653ab2537241e6eeaf668ab196e4d6e3d95
Chris Pressey <cpressey@catseye.tc>
2021-10-30 19:35:15 +0100

    Clone a fresh copy of the source before building an image from it.

--- script/build-docker-image.sh
+++ script/build-docker-image.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
+
 # Common script for building a Docker image.
 # Usage: build-docker-image.sh <dir-containing-docker-config>
 
 CONFIG_DIR=$1
-. $CONFIG_DIR/settings.sh
+if [ -r $CONFIG_DIR/settings.sh ]; then
+  . $CONFIG_DIR/settings.sh
+else
+  echo "'$CONFIG_DIR' is not a valid configuration directory"
+  exit 1
+fi
 
 shelf_pwd() {
     name="$1"
@@ -17,7 +24,7 @@ shelf_pwd() {
 }
 
 if shelf_pwd ${SOURCE}; then
-  SRCDIR=`shelf_pwd ${SOURCE}`
+  GITDIR=`shelf_pwd ${SOURCE}`
 else
   echo "Please set SHELF_PATH to a directory where the source '${SOURCE}' can be found."
   exit 1
@@ -27,8 +34,12 @@ ORGNAME=catseye
 DOCKERFILE=$CONFIG_DIR/Dockerfile
 IMAGENAME=$EXENAME
 
-find ${SRCDIR} -name '*.pyc' -exec rm {} \;
-find ${SRCDIR} -name '__pycache__' -exec rm -r {} \;
+SRCDIR=/tmp/$EXENAME
+
+echo "Cloning ${GITDIR} to ${SRCDIR}..."
+
+rm -rf ${SRCDIR}
+(cd /tmp/ && git clone ${GITDIR} ${EXENAME})
 
 docker container prune -f
 docker rmi ${ORGNAME}/${IMAGENAME}:${VERSION}
